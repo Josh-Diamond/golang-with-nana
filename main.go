1a987f02f50114b4ca381d18cd7f2778ee7feb73
Josh Keslar <diamondmail91@gmail.com>
2022-02-14 20:59:36 -0700

    slices

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	// var bookings []string
+	bookings := []string{}
+	var rsvp int
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -13,13 +16,12 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 	// array = [fixed length]datatype value
-	//var bookings = [50]string{"Nana", "Remy", "Josh"}
 
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
-	var rsvp uint = 1
+
 	// ask user for their name
 	fmt.Println("Enter your first name:")
 	fmt.Scan(&firstName)
@@ -31,9 +33,17 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	
+	bookings = append(bookings, firstName+" "+lastName)
+	rsvp += 1
+
+	fmt.Printf("The whole slice: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Printf("%v parties are attending this event\n", rsvp)
+	fmt.Printf("RSVP count: %v", rsvp)
+	fmt.Printf("All bookings: %v", bookings)
+
 }
